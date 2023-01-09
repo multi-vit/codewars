cbefc883805fc654e15db8282621fed417907018
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-01-09 17:17:01 +0000

    Add 7-kyu sum of first nth term of series solution

---
+++ javascript/7-kyu/sum-of-first-nth-term-of-series.js
@@ -0,0 +1,33 @@
+/*
+https://www.codewars.com/kata/555eded1ad94b00403000071
+
+Task:
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+*/
+
+function SeriesSum(n) {
+  let seriesNumber = 4;
+  let result = 1;
+  if (n === 0) {
+    return "0.00";
+  } else {
+    for (let i = 1; i < n; i++) {
+      result += 1 / seriesNumber;
+      seriesNumber += 3;
+    }
+  }
+  return result.toFixed(2).toString();
+}
